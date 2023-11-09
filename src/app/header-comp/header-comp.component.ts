27a5d6790a26cdcd2b102f56d6bec22090155d9b
Erik <59744487+knottem@users.noreply.github.com>
2023-11-09 22:45:41 +0100

    Started with header design added clock function to update every minute

--- src/app/header-comp/header-comp.component.ts
+++ src/app/header-comp/header-comp.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-comp',
   templateUrl: './header-comp.component.html',
   styleUrls: ['./header-comp.component.css']
 })
-export class HeaderCompComponent {
+export class HeaderCompComponent implements OnInit {
 
+  public currentTime: string = this.updateCurrentTime();
+  
+  constructor() { }
+
+  ngOnInit() {
+
+    // check every second to see if we need to update the time
+    setInterval(() => {
+      const newTime = this.updateCurrentTime();
+      if (this.currentTime !== newTime) {
+        this.currentTime = newTime;
+      }
+    }, 1000);
+    
+  }
+
+  // Updates the current time in HH:MM format, padded with leading zeros if necessary
+  private updateCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+ 
 }
