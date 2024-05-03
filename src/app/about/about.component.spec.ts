1a1bed34cd14274657b28ccd7446823223b0b1a3
Erik <59744487+knottem@users.noreply.github.com>
2024-05-03 10:06:14 +0200

    updated about component to be ready for translation

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
+import { TranslateModule } from "@ngx-translate/core";
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -8,10 +9,10 @@ describe('AboutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutComponent]
+      imports: [TranslateModule.forRoot()]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
