89df39bc7f51ae6b2abf6f16aa0bd4e6cfeb4134
Erik <59744487+knottem@users.noreply.github.com>
2024-06-01 14:33:27 +0200

    Updated tests to work with everything changed

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import {TranslateModule} from "@ngx-translate/core";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -8,10 +10,10 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchComponent]
+      imports: [TranslateModule.forRoot(), HttpClientTestingModule]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
