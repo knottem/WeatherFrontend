8852d9b90959c2cddfe8ac3b2411f745d0467e58
Erik <59744487+knottem@users.noreply.github.com>
2024-06-01 14:52:40 +0200

    just a path fix in config and minor ui change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'Weather App',
-  webDir: '/dist/weather-frontend',
+  webDir: 'dist/weather-frontend',
   plugins: {
     SplashScreen: {
       launchShowDuration: 1000,
