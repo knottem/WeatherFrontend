2e73b2198b4f5d9110380e798db3fb895e0bca07
Erik <59744487+knottem@users.noreply.github.com>
2024-11-12 16:41:18 +0100

    error comp updated for dark mode and added a small shake for errormessages if they are still active after a small delay

--- src/app/error-comp/error-comp.component.ts
+++ src/app/error-comp/error-comp.component.ts
@@ -8,12 +8,55 @@ import {ErrorService} from "../error.service";
 })
 export class ErrorCompComponent {
   @Input() errorMessage: string | null = null;
+  shakeClass: string = '';
+  shakeInterval: any;
+  shakeTimeout: any;
 
   constructor(private errorService: ErrorService) {}
 
-  dismissError() {
-    this.errorService.clearError();
+  ngOnInit() {
+    this.errorService.getError().subscribe(message => {
+      if (message) {
+        this.startDelayedShakeLoop();
+      } else {
+        this.stopShakeLoop();
+      }
+    });
   }
+
+  startDelayedShakeLoop() {
+    this.stopShakeLoop();
+    this.shakeTimeout = setTimeout(() => {
+      this.startShakeLoop();
+    }, 3000);
+  }
+
+  startShakeLoop() {
+    this.shakeClass = 'shake';
+    this.shakeInterval = setInterval(() => {
+      this.shakeClass = 'shake';
+      setTimeout(() => {
+        this.shakeClass = '';
+      }, 1000);
+    }, 3000);
+  }
+
+  stopShakeLoop() {
+    clearTimeout(this.shakeTimeout);
+    clearInterval(this.shakeInterval);
+    this.shakeClass = '';
+  }
+
+  dismissError() {
+    this.errorMessage = null;
+    this.errorService.clearError();
+    this.stopShakeLoop();
+  }
+
+  ngOnDestroy() {
+    this.stopShakeLoop();
+  }
+
 
 
 
