3c5c018f306bd4ee7c17fba71222d5f9361d9747
Erik <59744487+knottem@users.noreply.github.com>
2024-11-16 17:41:09 +0100

    updates to capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,23 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import {KeyboardResize} from "@capacitor/keyboard";
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'Weather App',
   webDir: 'dist/weather-frontend',
-  plugins: {},
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+      launchFadeOutDuration: 0,
+      backgroundColor: '#D9E9EA',
+      showSpinner: true,
+      spinnerColor: '#222428'
+    },
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      resizeOnFullScreen: true,
+    }
+  },
 };
 
 export default config;
