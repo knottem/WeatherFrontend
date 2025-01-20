36f934aedc9470234827b5237fd718d051b20486
Erik Wallenius <walle.o.w@gmail.com>
2025-01-20 17:55:23 +0100

    removed header clock and made the header always the same for both desktop and mobile

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -32,16 +32,4 @@ describe('HeaderCompComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('#updateCurrentTime should return a string in HH:MM format', () => {
-    const time = component.updateCurrentTime();
-    expect(time).toMatch(/^\d{2}:\d{2}$/);
-  });
-
-  it('#updateCurrentTime should return current time', () => {
-    const time = component.updateCurrentTime();
-    const now = new Date();
-    const expected = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`;
-    expect(time).toEqual(expected);
-  });
-
 });
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,15 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [`
-  `]
-
+  styles: [``]
 })
 export class HeaderComponent implements OnInit {
 
-  public currentTime: string = this.updateCurrentTime();
-  public isSmallScreen = false;
   public showHeader = true;
-
-  private intervalId!: ReturnType<typeof setInterval>;
 
   constructor(
     private router: Router,
@@ -23,15 +17,6 @@ export class HeaderComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.checkScreenSize();
-    // check every second to see if we need to update the time
-    this.intervalId = setInterval(() => {
-      const newTime = this.updateCurrentTime();
-      if (this.currentTime !== newTime) {
-        this.currentTime = newTime;
-      }
-    }, 1000);
-
     this.router.events.subscribe((event) => {
       if(event instanceof NavigationEnd) {
         this.showHeader = this.router.url !== '/search';
@@ -39,26 +24,8 @@ export class HeaderComponent implements OnInit {
     });
   }
 
-  ngOnDestroy() {
-    clearInterval(this.intervalId);
-  }
-
   openSearch() {
     this.router.navigate(['/search']);
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.checkScreenSize();
-  }
-
-  public checkScreenSize() {
-    this.isSmallScreen = window.innerWidth < 640; // Tailwind's `sm` breakpoint
-  }
-
-  public updateCurrentTime() {
-    const now = new Date();
-    return `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`;
   }
 
 }
